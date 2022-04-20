43213420cbe8e2af39ce4fcc2b4330efd051dbda
dimpdawzng <nsovipgl@gmail.com>
2022-04-20 09:11:04 +0700

    remake all

--- src/configs/connectDB.js
+++ src/configs/connectDB.js
@@ -1,17 +1,9 @@
-import mysql from "mysql2";
-var connection = mysql.createConnection({
+import mysql from "mysql2/promise";
+console.log("Creating connection pool...");
+var connection = mysql.createPool({
   host: "bcxvjyobemi2jcorkr23-mysql.services.clever-cloud.com",
   user: "uybxzhfo7oy1dnsu",
   password: "ckvObAChMIBZ6cYIc7T5",
   name: "bcxvjyobemi2jcorkr23",
-});
-
-connection.connect(function (err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-
-  console.log("connected as id " + connection.threadId);
 });
 export default connection;
