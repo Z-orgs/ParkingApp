0308db58de9b737288919e83a7ad833b1317a5ab
王世翰 <60683892+dimpDEV@users.noreply.github.com>
2022-04-27 23:51:39 +0700

    update database config

--- db-configs/db_table.sql
+++ db-configs/db_table.sql
@@ -2,7 +2,7 @@ CREATE TABLE userAdmin(
   idAdmin int AUTO_INCREMENT,
   userA varchar(30) NOT NULL UNIQUE,
   pword varchar(30) NOT NULL,
-  priceTyep1 int DEFAULT 3000,
+  priceType1 int DEFAULT 3000,
   priceType2 int DEFAULT 5000,
   priceType3 int DEFAULT 50000,
   priceType4 int DEFAULT 20000,
