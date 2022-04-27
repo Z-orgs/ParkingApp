9a03b6a38a84d89bab07723084ac67474589f504
王世翰 <60683892+dimpDEV@users.noreply.github.com>
2022-04-27 11:15:41 +0700

    nothing

--- src/configs/connectDB.js
+++ src/configs/connectDB.js
@@ -1,6 +1,6 @@
 import mysql from "mysql2/promise";
-import { host, user, password, database } from "./JSON/database.json"
-console.log("Creating connection pool...");
+import { host, user, password, database } from "./JSON/database.json";
+console.log("Connecting database...");
 var pool = mysql.createPool({
   host: host,
   user: user,
