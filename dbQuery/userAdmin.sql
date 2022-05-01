07384dbf6d56e4a41566fe81022cf31c72297387
王世翰 <60683892+dimpDEV@users.noreply.github.com>
2022-05-01 19:36:35 +0700

    fix userAdmin query

--- dbQuery/userAdmin.sql
+++ dbQuery/userAdmin.sql
@@ -1,10 +1,10 @@
 CREATE TABLE userAdmin(
-  idAdmin int AUTO_INCREMENT,
-  userA varchar(30) NOT NULL UNIQUE,
-  pword varchar(30) NOT NULL,
-  priceType1 int DEFAULT 3000,
-  priceType2 int DEFAULT 5000,
-  priceType3 int DEFAULT 50000,
-  priceType4 int DEFAULT 20000,
-  PRIMARY KEY(idAdmin, userA)
+	idAdmin int AUTO_INCREMENT,
+	userA varchar(1000) NOT NULL UNIQUE,
+	pword varchar(1000) NOT NULL,
+	priceType1 int DEFAULT 3000,
+	priceType2 int DEFAULT 5000,
+	priceType3 int DEFAULT 50000,
+	priceType4 int DEFAULT 20000,
+	PRIMARY KEY(idAdmin, userA)
 )
