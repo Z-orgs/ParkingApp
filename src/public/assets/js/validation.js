2d52078ada6794850fb9e2306a229cbfad8007fe
王世翰 <60683892+dimpDEV@users.noreply.github.com>
2022-06-14 02:51:21 +0700

    optimal

--- src/public/assets/js/validation.js
+++ src/public/assets/js/validation.js
@@ -1,7 +1,7 @@
 function Validator(formSelector) {
     function getParent(element, selector) {
-        while(element.parentElement) {
-            if(element.parentElement.matches(selector)) {
+        while (element.parentElement) {
+            if (element.parentElement.matches(selector)) {
                 return element.parentElement;
             }
             element = element.parentElement;
@@ -9,42 +9,42 @@ function Validator(formSelector) {
     }
     var formElement = document.querySelector(formSelector);
     var formRules = {};
-  
+
     var validatorRules = {
         required: function (value) {
             return value ? undefined : `Can't be left blank!`;
         },
         user_name: function (value) {
             const userNameRegex = /^[a-zA-Z0-9\_]+$/;
-            return userNameRegex.test(value) ? undefined : `Please re-enter your user name!`
+            return userNameRegex.test(value) ? undefined : `Please re-enter your user name!`;
         },
         min: function (min) {
             return function (value) {
-                return value.length >= min ? undefined : `Password min ${min} characters!`
-            }
+                return value.length >= min ? undefined : `Password min ${min} characters!`;
+            };
         },
         max: function (max) {
             return function (value) {
-                return value.length <= max ? undefined : `Password max ${max} characters!`
-            }
+                return value.length <= max ? undefined : `Password max ${max} characters!`;
+            };
         },
         comfirm: function (value) {
-            return value == document.querySelector('#password').value ? undefined : `Invalid confirmation password!`
+            return value == document.querySelector('#password').value ? undefined : `Invalid confirmation password!`;
         },
-        isFullName: function(value) {
+        isFullName: function (value) {
             const regexName = /^[a-zA-Z ]{2,}$/g;
-            return regexName.test(removeAscent(value)) ? undefined : `Full name invalid!`
+            return regexName.test(removeAscent(value)) ? undefined : `Full name invalid!`;
         },
-        isPhoneNumber: function(value) {
-            const regexPhoneNumber = /^(0?)(3[2-9]|5[6|8|9]|7[0|6-9]|8[0-6|8|9]|9[0-4|6-9])[0-9]{7}$/
-            return regexPhoneNumber.test(value) ? undefined : `Phone number invalid!`
+        isPhoneNumber: function (value) {
+            const regexPhoneNumber = /^(0?)(3[2-9]|5[6|8|9]|7[0|6-9]|8[0-6|8|9]|9[0-4|6-9])[0-9]{7}$/;
+            return regexPhoneNumber.test(value) ? undefined : `Phone number invalid!`;
         },
-        isNumber: function(value) {
-            const regex = /^[0-9]+$/
-            return regex.test(value) ? undefined : `Value must be numeric!`
+        isNumber: function (value) {
+            const regex = /^[0-9]+$/;
+            return regex.test(value) ? undefined : `Value must be numeric!`;
         }
     };
-    
+
     // Nếu tồn tại formElement
     if (formElement) {
         var inputs = formElement.querySelectorAll('[name][rules]');
@@ -54,8 +54,8 @@ function Validator(formSelector) {
                 rule.split(':');
                 var ruleInfo;
                 var isRuleHasValue = rule.includes(':');
-  
-                if(isRuleHasValue) {
+
+                if (isRuleHasValue) {
                     ruleInfo = rule.split(':');
                     rule = ruleInfo[0];
                 }
@@ -63,20 +63,20 @@ function Validator(formSelector) {
                 if (isRuleHasValue) {
                     ruleFn = ruleFn(ruleInfo[1]);
                 }
-                if(Array.isArray(formRules[input.name])) {
+                if (Array.isArray(formRules[input.name])) {
                     formRules[input.name].push(ruleFn);
                 } else {
                     formRules[input.name] = [ruleFn];
                 }
             }
-  
+
             //Xử lí khi blur, click...
             input.onblur = handleValidate;
-  
+
             input.oninput = handleClearError;
         }
-  
-        
+
+
         function handleValidate(e) {
             rules = formRules[e.target.name];
             var errorMsg;
@@ -84,44 +84,44 @@ function Validator(formSelector) {
                 errorMsg = rule(e.target.value);
                 if (errorMsg) break;
             }
-  
+
             //Hiện thị lỗi ra UI
             if (errorMsg) {
                 var formGroup = getParent(e.target, '.form-group');
                 if (formGroup) {
                     formGroup.classList.add('invalid');
-                    var formError = formGroup.querySelector('.error-msg')
+                    var formError = formGroup.querySelector('.error-msg');
                     if (formError) {
                         formError.innerHTML = errorMsg;
                     }
-  
+
                 }
             }
             return !errorMsg;
         }
-  
+
         function handleClearError(e) {
             var formGroup = getParent(e.target, '.form-group');
             if (formGroup.classList.contains('invalid')) {
                 formGroup.classList.remove('invalid');
-                var formError = formGroup.querySelector('.error-msg')
-                    if (formError) {
-                        formError.innerHTML = '';
-                    }
+                var formError = formGroup.querySelector('.error-msg');
+                if (formError) {
+                    formError.innerHTML = '';
+                }
             }
         }
 
-        function removeAscent (str) {
+        function removeAscent(str) {
             if (str === null || str === undefined) return str;
-             str = str.toLowerCase();
-             str = str.replace(/à|á|ạ|ả|ã|â|ầ|ấ|ậ|ẩ|ẫ|ă|ằ|ắ|ặ|ẳ|ẵ/g, "a");
-             str = str.replace(/è|é|ẹ|ẻ|ẽ|ê|ề|ế|ệ|ể|ễ/g, "e");
-             str = str.replace(/ì|í|ị|ỉ|ĩ/g, "i");
-             str = str.replace(/ò|ó|ọ|ỏ|õ|ô|ồ|ố|ộ|ổ|ỗ|ơ|ờ|ớ|ợ|ở|ỡ/g, "o");
-             str = str.replace(/ù|ú|ụ|ủ|ũ|ư|ừ|ứ|ự|ử|ữ/g, "u");
-             str = str.replace(/ỳ|ý|ỵ|ỷ|ỹ/g, "y");
-             str = str.replace(/đ/g, "d");
-             return str;
-         }
+            str = str.toLowerCase();
+            str = str.replace(/à|á|ạ|ả|ã|â|ầ|ấ|ậ|ẩ|ẫ|ă|ằ|ắ|ặ|ẳ|ẵ/g, "a");
+            str = str.replace(/è|é|ẹ|ẻ|ẽ|ê|ề|ế|ệ|ể|ễ/g, "e");
+            str = str.replace(/ì|í|ị|ỉ|ĩ/g, "i");
+            str = str.replace(/ò|ó|ọ|ỏ|õ|ô|ồ|ố|ộ|ổ|ỗ|ơ|ờ|ớ|ợ|ở|ỡ/g, "o");
+            str = str.replace(/ù|ú|ụ|ủ|ũ|ư|ừ|ứ|ự|ử|ữ/g, "u");
+            str = str.replace(/ỳ|ý|ỵ|ỷ|ỹ/g, "y");
+            str = str.replace(/đ/g, "d");
+            return str;
+        }
     }
 }
