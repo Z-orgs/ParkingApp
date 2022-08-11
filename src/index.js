20a99bd62066a6205e5714a13e066fea28cbe7a5
drmXYZ <nsovipgl@gmail.com>
2022-08-11 16:49:38 +0700

    remove .env

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import express from "express";
 import configViewEngine from "./configs/viewEngine.js";
 import initWebRoute from "./route/web.js";
-import herokuAwake from "heroku-awake";
 const app = express();
-const url = "http://parkingapp.xyz";
 const PORT = process.env.PORT || 80;
 configViewEngine(app);
 initWebRoute(app);
@@ -11,8 +9,5 @@ app.use((req, res) => {
   return res.redirect("/");
 });
 app.listen(PORT, () => {
-  herokuAwake(url);
-  const time = 10;
-  herokuAwake(url, time);
   console.log(`OK: ${PORT}`);
 });
